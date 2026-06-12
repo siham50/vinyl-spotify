9421a90fd67e11b9957b76336cea91b0f8a082f8
siham <siham.emf35@gmail.com>
2026-06-12 17:06:29 +0100

    using HTTPS instead of HTTP

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), basicSsl(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
